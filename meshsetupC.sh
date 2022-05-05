de3a9920149963515ca740c505fbef912a5837f7
John White <johnwhite379@gmail.com>
2022-05-05 15:39:59 -0400

    mesh setup for curved pipe

---
+++ meshsetupC.sh
@@ -0,0 +1,16 @@
+cd ./curvedProfiles/
+
+for FILE in *
+do
+    cp -R ../bmdc ../exampleCase/system/blockMeshDict
+    python3 ../pythonScripts/blockMeshgen.py <<ENDOF
+./${FILE}
+ENDOF
+    key=$(python3 ../pythonScripts/keygen.py)    
+    rm -rf ../patientCases/${FILE:0:7}${key}
+    cp -R ../exampleCase/ ../patientCases/${FILE:0:7}${key}
+    mv ${FILE} ${FILE:0:7}${key}.csv
+    cd ../patientCases/${FILE:0:7}${key}/
+    blockMesh >bmlog
+    cd -
+done
